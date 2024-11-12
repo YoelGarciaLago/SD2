ad8c38be11e8546a095c24d2a9cc7843bf2ea055
YoelGarciaLago <145323567+YoelGarciaLago@users.noreply.github.com>
2024-11-12 12:40:41 +0100

    Enum de los colores

--- app/src/main/java/com/example/experimento/Datos.kt
+++ app/src/main/java/com/example/experimento/Datos.kt
@@ -1,4 +1,10 @@
 package com.example.experimento
 
+import androidx.compose.ui.graphics.Color
+
 object Datos {
+    var _numbers: Int = 0
+    enum class coloresJuego(rgb: Int, colorBoton: Color){
+        ROJO(1, Color.Red),AZUL(2, Color.Blue),VERDE(3, Color.Green),GRIS(4, Color.Gray);
+    }
 }
