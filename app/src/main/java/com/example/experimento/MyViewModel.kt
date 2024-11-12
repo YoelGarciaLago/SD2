dbf7fa1ac033eaad7311dee3c6d9edf0a619109b
YoelGarciaLago <145323567+YoelGarciaLago@users.noreply.github.com>
2024-11-12 12:45:00 +0100

    ViewModel con un par de funciones.

--- app/src/main/java/com/example/experimento/MyViewModel.kt
+++ app/src/main/java/com/example/experimento/MyViewModel.kt
@@ -1,4 +1,28 @@
 package com.example.experimento
 
-class MyViewModel {
+import android.util.Log
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableStateOf
+import androidx.lifecycle.ViewModel
+
+class MyViewModel : ViewModel() {
+    val TAG_LOG : String = "Mi_debug"
+    var _numbers = mutableStateOf(0)
+    var texto: MutableState<String> = mutableStateOf("Numero generado")
+    var isCorrect: MutableState<Boolean?> = mutableStateOf(null)
+
+    fun crearRandomBoton() {
+        val randomButtonIndex = (1..4).random()
+        Datos._numbers = randomButtonIndex
+        Log.d(TAG_LOG, "Random: $randomButtonIndex") // Muestra en el Log el número aleatorio
+    }
+
+    fun compararColores(numColor: Int): Boolean {
+        val resultado = numColor == Datos._numbers
+        isCorrect.value = resultado  // Actualiza isCorrect y Compose detectará el cambio
+        Log.d(TAG_LOG,"Resultado: " + resultado)
+        // Actualiza el texto según el resultado
+        texto.value = if (resultado) "Correcto" else "Incorrecto"
+        return resultado
+    }
 }
