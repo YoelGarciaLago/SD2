e85314ca802c9b49d572cc0199d015ae18cc3c66
Yoel García Lago <ygarcialago@danielcastelao.org>
2024-11-27 12:37:38 +0100

    Clases comentadas (falta la IU)

--- app/src/main/java/com/example/experimento/Datos.kt
+++ app/src/main/java/com/example/experimento/Datos.kt
@@ -4,9 +4,13 @@ import androidx.compose.ui.graphics.Color
 import androidx.lifecycle.MutableLiveData
 
 object Datos {
+    //Número que se generó random
     var _number: Int = 0
+    // Número de la ronda
     val rondas : MutableLiveData<Int> = MutableLiveData(1)
 }
+
+// Enum de la info de los botones. Color --> le da el color al boton. txt --> da el texto que tiene el Boton. valorNumerico --> da el valor a los botones, el que se usa para comparar el número que tiene el boton con el generado random
 enum class coloresJuego(val colorBoton: Color, val txt: String, val valorNumerico: Int){
     ROJO(Color.Red, "Rojo",1),
     AZUL(Color.Blue,"Azul",2),
@@ -14,11 +18,14 @@ enum class coloresJuego(val colorBoton: Color, val txt: String, val valorNumeric
     GRIS(Color.Gray,"Gris",4),
     AMARILLO_START(Color.Yellow,"Start",0);
 }
+//Estados por los que pasa el juego
 enum class EstadosJuego(val start_activo: Boolean, val boton_activo: Boolean){
     INICIO(true,false),
     GENERANDO(false,false),
     ADIVINANDO(false,true)
 }
+
+//Estados que se usaron para la cuenta atrás y para el método de estados auxiliares
 enum class EstadosAuxiliares(val txt: String, val numeroTxT: String) {
     AUX5("aux5","5"),
     AUX4("aux4","4"),
--- app/src/main/java/com/example/experimento/MyViewModel.kt
+++ app/src/main/java/com/example/experimento/MyViewModel.kt
@@ -10,20 +10,26 @@ import kotlinx.coroutines.launch
 
 class MyViewModel : ViewModel() {
 
+    //Tag para Debuguear
     val TAG_LOG : String = "Mi_debug"
+    //Varaible para que genere un random y se lo pase a Datos
     var _numbers = mutableStateOf(0)
+    //Variable que controla el estado del juego
     var estadoDelJuego : MutableLiveData<EstadosJuego> = MutableLiveData(EstadosJuego.INICIO)
-    private val TAG_LOG_CUENTA = "miDebug_Cuenta"
 
 
+//Nombre bastante descriptivo
     fun crearRandom(){
+        //Pasa el estado a Generando
         estadoDelJuego.value = EstadosJuego.GENERANDO
+    //Crea el random entre los límites de los valores de los botones
         _numbers.value = (1..4).random()
-        //Datos._number = _numbers.value
+        //Log para poder ver como va el programa
         Log.d(TAG_LOG, "Valor random creado --> ${_numbers.value} - Estado --> ${estadoDelJuego.value}")
         pasarRandom()
     }
 
+    //Función que pasa el random creado en la variable de esta clase para la varaible de datos
     fun pasarRandom(){
         Log.d(TAG_LOG,"Pasamos la variable al object Datos - Estado --> ${estadoDelJuego.value}")
         Datos._number = _numbers.value
@@ -31,22 +37,28 @@ class MyViewModel : ViewModel() {
     }
 
     fun comparar(nComparar: Int): Boolean {
-        //estadosAuxiliares()
 
         Log.d(TAG_LOG, "Comprobación de la variable - Estado --> ${estadoDelJuego.value}")
+
+        //Return que tiene condiciones. Esta es condición que mira si es igual
         return if (nComparar.equals(Datos._number)){
-            estadoDelJuego.value = EstadosJuego.INICIO
+            //Suma una ronda
             Datos.rondas.value = Datos.rondas.value?.plus(1)
             Log.d(TAG_LOG,"Valor de ronda: ${Datos.rondas.value}")
             Log.d(TAG_LOG,"Has acertado - Estado --> ${estadoDelJuego.value}")
+            //devuelve true para que se actualice en la IU
             true
         }else{
+            //Pasa el juego al inicio
             estadoDelJuego.value = EstadosJuego.INICIO
+            //Reinicia las rondas
             Datos.rondas.value = 1
             Log.d(TAG_LOG,"No has acertado - Estado --> ${estadoDelJuego.value}")
             false
         }
     }
+
+    //Método auxiliar para utilizado anteriormente
     fun estadosAuxiliares(){
         viewModelScope.launch(){
             var estadosAux : EstadosAuxiliares = EstadosAuxiliares.AUX1
@@ -68,6 +80,7 @@ class MyViewModel : ViewModel() {
         }
     }
 
+    //Método para poder pasarle el valor de las rondas a la IU
     fun getRondaValue(): MutableLiveData<Int>{
         return Datos.rondas
     }
